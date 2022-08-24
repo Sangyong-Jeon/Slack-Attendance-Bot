5550e42aba2d49a281ed9c7024326d2478050f32
JeonSangYong <tkddyd420@naver.com>
2022-08-24 10:48:48 +0900

    [BE] 스프링부트 생성

---
+++ backend/slack-attendance-bot/src/main/java/shop/studyletme/attendance/SlackAttendanceBotApplication.java
@@ -0,0 +1,13 @@
+package shop.studyletme.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackAttendanceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackAttendanceBotApplication.class, args);
+    }
+
+}
---
+++ backend/slack-attendance-bot/src/test/java/shop/studyletme/attendance/SlackAttendanceBotApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.studyletme.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackAttendanceBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
